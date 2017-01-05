8df79f52380629d59566a9b17660064aa8259bf2
Mark Glover <mark.glover@irap.org>
2017-01-05 10:32:52 +0000

    Adds encryption to user password in the getUserToken method

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -12,9 +12,10 @@ class AuthController extends AbstractResourceController
 {
     public static function getUserToken($email, $password)
     {
+        $encrypted_password = self::encrypt($password, \iRAP\VidaSDK\APP_PRIVATE_KEY);
         $request = new \iRAP\VidaSDK\Models\APIRequest();
         $request->setUrl("auth/register");
-        $request->setPostData(array("email"=>$email,"password"=>$password));
+        $request->setPostData(array("email"=>$email,"password"=>$encrypted_password));
         $request->send();
         return parent::response($request);
     }
@@ -33,5 +34,25 @@ class AuthController extends AbstractResourceController
         $request->setPostData(array("name"=>$name,"owner"=>$owner));
         $request->send();
         return parent::response($request);
+    }
+    
+    /**
+     * Encrypt a String
+     * @param String $message - the message to encrypt
+     * @param String $key - the key to encrypt and then decrypt the message.
+     * @return String - the encryptd form of the string
+     */
+    private static function encrypt($message, $key)
+    {
+        $md5Key = md5($key);
+        
+        $encrypted = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, 
+                                    $md5Key, 
+                                    $message, 
+                                    MCRYPT_MODE_CBC, 
+                                    md5($md5Key));
+        
+        $encoded_encryption = base64_encode($encrypted);
+        return $encoded_encryption;
     }
 }
