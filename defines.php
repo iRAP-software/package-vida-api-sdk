fd54a384fc0b82c971cd35a04fefe9829d7b21e8
Mark Glover <mark.glover@irap.org>
2017-06-27 10:09:38 +0100

    Updates API host name

--- defines.php
+++ defines.php
@@ -5,7 +5,7 @@
  */
 namespace iRAP\VidaSDK;
 
-define(__NAMESPACE__ . '\IRAP_API_LIVE_URL', 'http://sp-fixes-api.release.vida.irap.org');
+define(__NAMESPACE__ . '\IRAP_API_LIVE_URL', 'http://api.release.vida.irap.org');
 
 define(__NAMESPACE__ . '\IRAP_API_VERSION', '1.0');
 
