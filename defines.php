f1ea230942357088d2172dfd12dcb5dea6c83d1e
Programster <sdpagent@gmail.com>
2017-11-24 15:42:49 +0000

    Switching from defines to static constants.

--- defines.php
+++ defines.php
@@ -2,12 +2,14 @@
 
 /* 
  * List of defines to use in the ViDA SDK
+ * These were converted from literal defines to constants in a defines class for autoloading.
  */
 namespace iRAP\VidaSDK;
 
-define(__NAMESPACE__ . '\IRAP_API_LIVE_URL', 'https://api.vida.irap.org');
 
-define(__NAMESPACE__ . '\IRAP_API_VERSION', '1.0');
-
-
-define(__NAMESPACE__ . '\IRAP_PERMISSIONS_LIVE_URL', 'https://sso.irap.org/permissions/request_api_permissions');
+class Defines
+{
+    const IRAP_API_LIVE_URL = 'https://api.vida.irap.org';
+    const IRAP_API_VERSION = "1.0";
+    const IRAP_PERMISSIONS_LIVE_URL = 'https://sso.irap.org/permissions/request_api_permissions';
+}
