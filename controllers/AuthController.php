6ddbe26f77882e6545128bda57e345b9caf4bafc
SamratRai <samrat.rai@irap.org>
2023-08-03 15:38:32 +0100

    Undo cleanup

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -3,23 +3,17 @@
 /**************************************************************************************************
  * This file is for internal use by the ViDA SDK. It should not be altered by users
  **************************************************************************************************
- * 
+ *
  * Contains the authentication methods used to request and set tokens. Also handles encryption
  * of the user password, before it is transmitted to the API
- * 
+ *
  */
 
 namespace iRAP\VidaSDK\Controllers;
 
-use iRAP\VidaSDK\Defines;
-use iRAP\VidaSDK\Models\AbstractAuthentication;
-use iRAP\VidaSDK\Models\APIRequest;
-use iRAP\VidaSDK\Models\Response;
-use JetBrains\PhpStorm\NoReturn;
-
 class AuthController extends AbstractResourceController
 {
-    protected function getResourcePath(): string
+    protected function getResourcePath()
     {
         return 'auth';
     }
@@ -29,44 +23,43 @@ class AuthController extends AbstractResourceController
      * should be stored locally and used for all future requests. Email and password should NOT
      * be stored locally. The password is encrypted using the APP_PRIVATE KEY before transmission.
      *
-     * @param AbstractAuthentication $auth
      * @param string $email
      * @param string $password
-     * @return Response
+     * @return object
      */
-    public static function getUserToken(AbstractAuthentication $auth, string $email, string $password): Response
+    public static function getUserToken(\iRAP\VidaSDK\Models\AbstractAuthentication $auth, $email, $password)
     {
         $encrypted_password = $auth->getEncryption($password);
-        $request = new APIRequest($auth);
+        $request = new \iRAP\VidaSDK\Models\APIRequest($auth);
         $request->setUrl("auth/register");
         $request->setPostData(array("email"=>$email,"password"=>$encrypted_password));
         $request->send();
         return parent::response($request);
     }
-    
+
     /**
-     * Builds query parameters to sign, signs them and then sends the query to ViDA, so that the 
+     * Builds query parameters to sign, signs them and then sends the query to ViDA, so that the
      * user can view and accept/reject the permissions that the app is asking for.
-     * 
-     * @param AbstractAuthentication $auth
-     * @param string $returnUrl
+     *
+     * @param \iRAP\VidaSDK\Models\AbstractAuthentication $auth
+     * @param type $returnUrl
      */
-    #[NoReturn] public static function requestUserPermissions(AbstractAuthentication $auth, string $returnUrl): void
+    public static function requestUserPermissions(\iRAP\VidaSDK\Models\AbstractAuthentication $auth, $returnUrl)
     {
         $headers = $auth->getAuthHeaders();
         $headers['return_url'] = urlencode($returnUrl);
         $signature = $auth->getSignatures($headers);
         $query = array_merge($headers, $signature);
-        
+
         if (defined('\iRAP\VidaSDK\IRAP_PERMISSIONS_URL'))
         {
             $url = \iRAP\VidaSDK\IRAP_PERMISSIONS_URL;
         }
         else
         {
-            $url = Defines::IRAP_PERMISSIONS_LIVE_URL;
+            $url = \iRAP\VidaSDK\Defines::IRAP_PERMISSIONS_LIVE_URL;
         }
-        
+
         header('Location: ' . $url . '?' . http_build_query($query));
         die();
     }
