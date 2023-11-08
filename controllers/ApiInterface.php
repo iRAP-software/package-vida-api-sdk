bba07b12539e351277c0936bcd2d46a17386cb59
Anne Alexander <37503494+annie917@users.noreply.github.com>
2023-11-08 15:16:25 +0000

    VIDA-2817 - Road classifications moved to internal sdk

--- controllers/ApiInterface.php
+++ controllers/ApiInterface.php
@@ -250,10 +250,4 @@ interface ApiInterface
     public function addManager(string $identifier, int $userId);
 
     public function deleteManager(string $identifier, int $userId);
-
-    public function getRoadClassificationRulesets(int $id = Null);
-
-    public function deleteRoadClassificationRuleset(int $id);
-
-    public function updateRoadClassificationStatus(int $id, string $status);
 }
